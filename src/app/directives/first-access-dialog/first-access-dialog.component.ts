8cb0f9d03ad278612e755de9e200529b1ad05605
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-06-01 12:25:38 +0200

    Ms

--- src/app/directives/first-access-dialog/first-access-dialog.component.ts
+++ src/app/directives/first-access-dialog/first-access-dialog.component.ts
@@ -10,15 +10,15 @@ import { animate, style, transition, trigger } from "@angular/animations";
   templateUrl: './first-access-dialog.component.html',
   styleUrls: ['./first-access-dialog.component.scss'],
   animations: [
-    // trigger('pageAnimation', [
-    //   transition(':enter', [
-    //     style({ opacity: 0, transform: 'translateX(100%)' }),
-    //     animate('300ms ease-in', style({ opacity: 1, transform: 'translateX(0)' }))
-    //   ]),
-    //   transition(':leave', [
-    //     animate('300ms ease-out', style({ opacity: 0, transform: 'translateX(-100%)' }))
-    //   ])
-    // ])
+    trigger('pageAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(100%)' }),
+        animate('300ms ease-in', style({ opacity: 1, transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ opacity: 0, transform: 'translateX(-100%)' }))
+      ])
+    ])
   ]
 })
 export class FirstAccessDialogComponent {
