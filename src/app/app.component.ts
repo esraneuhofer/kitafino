5cd9ef3f7368de9ce2195a41c86979acfbeb8cde
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-06-14 12:45:47 +0200

    mas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,17 @@ export class AppComponent implements OnInit{
     this.languageService.setLanguage(language);
   }
   ngOnInit() {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/ngsw-worker.js')
+          .then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch(error => {
+            console.error('Service Worker registration failed:', error);
+          });
+      });
+    }
     this.apiService.setLanguage({lang:'en'}).subscribe(
       data => {
         console.log('Data received in component:', data);
