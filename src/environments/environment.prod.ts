442fc60779f42c29ede0f4a9a125893e00d67ced
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-06-17 13:00:43 +0200

    ssd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://kitafino-45139aec3e10.herokuapp.com/api'
-
-};
 // export const environment = {
 //   production: true,
-//   apiBaseUrl: '/api'
+//   apiBaseUrl: 'https://kitafino-45139aec3e10.herokuapp.com/api'
+//
 // };
+export const environment = {
+  production: true,
+  apiBaseUrl: '/api'
+};
