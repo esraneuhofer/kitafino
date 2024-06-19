a3be6963f3e5e42b4ecf26605cff87778ea5f5ea
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-06-19 22:16:59 +0200

    mS

--- server/controllers/help.controller.js
+++ server/controllers/help.controller.js
@@ -6,7 +6,6 @@ const Help = mongoose.model('HelpSchema');
 module.exports.getSingleHelpPdfBase = async (req, res, next) => {
   try {
     // Using await to wait for the result of Tenant.find()
-    console.log(req.query.routeName)
     const HelpModel = await Help.findOne({ 'routeName': req.query.routeName });
     // Sending the result back to the client
     res.json(HelpModel);
@@ -20,7 +19,7 @@ module.exports.getSingleHelpPdfBase = async (req, res, next) => {
 module.exports.getAllHelpPdfNames = async (req, res, next) => {
   try {
     // Using await to wait for the result of Tenant.find()
-    const HelpModel = await Help.find({},'filename routeName');
+    const HelpModel = await Help.find({},'nameFile routeName');
     // Sending the result back to the client
     res.json(HelpModel);
   } catch (err) {
--- src/app/home/help/help.component.ts
+++ src/app/home/help/help.component.ts
@@ -1,4 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {downloadPdfHelpIos} from "../weekplan-pdf/download-ios.function";
+import {downloadPdfWeb} from "../weekplan-pdf/download-web.function";
+import {HelpPdfInterface, HelpService} from "../../service/help.service";
+import {PlatformService} from "../../service/platform.service";
+import {FileOpener} from "@ionic-native/file-opener/ngx";
+import {LanguageService} from "../../service/language.service";
 
 export interface HelpDocument {
     nameDocument: string;
@@ -13,19 +19,40 @@ export interface HelpDocument {
 })
 export class HelpComponent implements OnInit {
 
+  page = 1;
+  pageSize = 7;
     pageLoaded: boolean = false;
-    helpDocuments: any[] = [];
+    helpDocuments: HelpPdfInterface[] = [];
     submittingRequest: boolean = false;
-
-    constructor() {
+    lang: string = 'de'
+  isApp: boolean = false;
+    constructor(private helpService: HelpService,
+                private fileOpener: FileOpener,
+                private platformService: PlatformService,
+                private languageService: LanguageService,) {
     }
 
     ngOnInit() {
         this.pageLoaded = true;
+      this.isApp = this.platformService.isIos || this.platformService.isAndroid;
+        this.lang = this.languageService.getLanguage();
+
+        this.helpService.getAllHelpPdfNames().subscribe((data: HelpPdfInterface[]) => {
+            this.helpDocuments = data;
+        })
     }
 
-    selectDocument(document: any) {
 
-    }
+  async openHelpPdf(helpDocument: HelpPdfInterface) {
+    this.helpService.getSingleHelpPdfBase({routeName:helpDocument.routeName}).subscribe(async (data: any) => {
+      console.log(data);
+      if (this.isApp) {
+        await downloadPdfHelpIos(data, this.fileOpener);
+      } else {
+        downloadPdfWeb(data);
+      }
+    });
+  }
+
 
 }
