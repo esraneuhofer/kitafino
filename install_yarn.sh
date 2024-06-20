fd960974430f68952842eb2e2c2e5b4bf51cc761
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-06-20 13:48:39 +0200

    mS

--- install_yarn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -o- -L https://yarnpkg.com/install.sh | bash
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-echo 'export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"' >> $HOME/.profile
