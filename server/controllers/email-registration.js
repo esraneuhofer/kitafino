f41f52c0337ac6df05be17ae8ffc14cf9888144b
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-06-21 08:48:27 +0200

    Ms

--- server/controllers/email-registration.js
+++ server/controllers/email-registration.js
@@ -1,9 +1,9 @@
 const i18n = require('i18n');
 
-function getHtmlRegistrationEmail(username, password,lang) {
+function getHtmlRegistrationEmail(username, password) {
   return `
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="${i18n.getLocale(lang)}">
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="de">
   <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1" name="viewport">
