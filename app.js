cf53c5c7793dec3006c540d45fa2efa041f5df54
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-07-16 15:11:59 +0200

    Setting Seperate

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 var app = express();
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
-const { Server } = require('socket.io');
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
