290346d580f2cf16f454fc87f5c60d9c6a9b1eaf
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-08-23 15:21:52 +0200

    Feedback

---
+++ src/app/home/feedback/feedback.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {GenerellService} from "../../service/generell.service";
+import {MessageService} from "../../service/message.service";
+import {MessageDialogService} from "../../service/message-dialog.service";
+import {NgForm} from "@angular/forms";
+import {TranslateService} from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.scss']
+})
+export class FeedbackComponent implements OnInit {
+
+  pageLoaded = false;
+  submittingRequest = false;
+  feedbackModel = {
+    message: ''
+  }
+
+  constructor(private generalService: GenerellService,
+              private translate: TranslateService,
+              private messageService: MessageDialogService) {
+
+  }
+
+  ngOnInit() {
+    this.pageLoaded = true;
+  }
+
+  sendFeedback(form: NgForm) {
+    this.submittingRequest = true;
+    this.generalService.sendFeedback(this.feedbackModel).subscribe(
+      () => {
+        this.submittingRequest = false;
+        form.resetForm()
+        this.feedbackModel.message = '';
+        this.messageService.openMessageDialog(
+          this.translate.instant("DIALOG_FEEDBACK_MESSAGE"),
+          this.translate.instant("THANK_YOU"), 'success');
+      },
+      () => {
+        this.submittingRequest = false;
+      }
+    )
+  }
+}
