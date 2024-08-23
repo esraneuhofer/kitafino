31592c64319e6287e17e15e891070c7f80bff832
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-08-23 16:06:48 +0200

    Ms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.initializeApp();
+    // this.initializeApp();
     this.apiService.setLanguage({ lang: 'en' }).subscribe(
       data => {
         // console.log('Data received in component:', data);
@@ -36,39 +36,39 @@ export class AppComponent implements OnInit {
     );
   }
 
-  async initializeApp() {
-    this.platform.ready().then(async () => {
-      await this.checkNetworkStatus();
-    });
-  }
-
-  async checkNetworkStatus() {
-    const status = await Network.getStatus();
-
-    if (!status.connected) {
-      this.showNoNetworkAlert();
-    }
-
-    // Event Listener für Netzwerkstatus-Änderungen hinzufügen
-    Network.addListener('networkStatusChange', (status) => {
-      if (!status.connected) {
-        this.showNoNetworkAlert();
-      } else {
-        this.hideNetworkAlert();
-      }
-    });
-
-    SplashScreen.hide(); // Splashscreen ausblenden, nachdem die Netzwerküberprüfung abgeschlossen ist
-  }
-
-  showNoNetworkAlert() {
-    alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
-    // Hier kannst du auch eine benutzerdefinierte Anzeige oder ein Modal öffnen
-  }
-
-  hideNetworkAlert() {
-    // Logik zum Schließen der Netzwerk-Warnung oder eines Modals, falls erforderlich
-  }
+  // async initializeApp() {
+  //   this.platform.ready().then(async () => {
+  //     await this.checkNetworkStatus();
+  //   });
+  // }
+  //
+  // async checkNetworkStatus() {
+  //   const status = await Network.getStatus();
+  //
+  //   if (!status.connected) {
+  //     this.showNoNetworkAlert();
+  //   }
+  //
+  //   // Event Listener für Netzwerkstatus-Änderungen hinzufügen
+  //   Network.addListener('networkStatusChange', (status) => {
+  //     if (!status.connected) {
+  //       this.showNoNetworkAlert();
+  //     } else {
+  //       this.hideNetworkAlert();
+  //     }
+  //   });
+  //
+  //   SplashScreen.hide(); // Splashscreen ausblenden, nachdem die Netzwerküberprüfung abgeschlossen ist
+  // }
+  //
+  // showNoNetworkAlert() {
+  //   alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
+  //   // Hier kannst du auch eine benutzerdefinierte Anzeige oder ein Modal öffnen
+  // }
+  //
+  // hideNetworkAlert() {
+  //   // Logik zum Schließen der Netzwerk-Warnung oder eines Modals, falls erforderlich
+  // }
 
 
 
