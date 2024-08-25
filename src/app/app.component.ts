bba90280795d957e8a505ac363c3dd10e135e4ba
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-08-25 14:30:01 +0200

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit {
   onOnline(event: Event) {
     this.isOnline = true;
     console.log('Online!');
-    alert('Internetverbindung. ');
+    alert('Internetverbindung wieder hergestellt.');
     // Handle going online
   }
 
