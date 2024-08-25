339d49bf92fe1eef344994b99f2461099503e7a1
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-08-25 14:58:31 +0200

    S

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import { LanguageService } from "./service/language.service";
-import { ApiService } from "./service/api.service";
-import { environment } from "../environments/environment";
-import { ToastingService } from "./service/toastr.service";
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@capacitor/splash-screen';
-import {Platform} from "@ionic/angular";
+import { LanguageService } from './service/language.service';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
@@ -12,21 +10,37 @@ import {Platform} from "@ionic/angular";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  isOnline: boolean = navigator.onLine;  // Initialer Online-Status basierend auf dem aktuellen Navigator-Status
 
-  networkListener: any
-  isOnline: boolean = navigator.onLine;
+  constructor(
+    private platform: Platform,
+    private languageService: LanguageService,
+    private apiService: ApiService
+  ) {}
 
-  constructor(private toastr: ToastingService,
-              private platform: Platform,
-              private languageService: LanguageService,
-              private apiService: ApiService) {
-    // console.log(`Environment API Base URL: ${environment.apiBaseUrl}`);  // Log the environment variable directly
+  ngOnInit() {
+    this.checkInitialNetworkStatus();
+    this.initializeApp();
+    console.log('App component initialized!');
+
+    // API-Aufruf basierend auf dem initialen Netzwerkstatus
+    if (this.isOnline) {
+      this.apiService.setLanguage({ lang: 'en' }).subscribe(
+        data => {
+          console.log('Data received in component:', data);
+        },
+        error => {
+          console.error('Error in component:', error);
+        }
+      );
+    }
   }
 
-  switchLanguage(language: string): void {
-    this.languageService.setLanguage(language);
+  async initializeApp() {
+    this.platform.ready().then(async () => {
+      SplashScreen.hide();
+    });
   }
-
 
   // Event Listener für Netzwerkstatus-Änderungen
   @HostListener('window:online', ['$event'])
@@ -34,7 +48,7 @@ export class AppComponent implements OnInit {
     this.isOnline = true;
     console.log('Online!');
     alert('Internetverbindung wieder hergestellt.');
-    // Handle going online
+    // Hier können weitere Aktionen hinzugefügt werden, wenn die App wieder online ist
   }
 
   @HostListener('window:offline', ['$event'])
@@ -42,7 +56,18 @@ export class AppComponent implements OnInit {
     this.isOnline = false;
     console.log('Offline!');
     alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
-    // Handle going offline
+    // Hier können weitere Aktionen hinzugefügt werden, wenn die App offline geht
+  }
+
+  checkInitialNetworkStatus() {
+    // Überprüft den initialen Netzwerkstatus beim Start der App
+    if (!navigator.onLine) {
+      this.isOnline = false;
+      this.updateNetworkStatus();
+    } else {
+      this.isOnline = true;
+      this.updateNetworkStatus();
+    }
   }
 
   updateNetworkStatus() {
@@ -53,26 +78,8 @@ export class AppComponent implements OnInit {
       alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
     }
   }
-  ngOnInit() {
-    this.updateNetworkStatus();
-    this.initializeApp();
-    console.log('App component initialized!');
-    this.apiService.setLanguage({ lang: 'en' }).subscribe(
-      data => {
-        // console.log('Data received in component:', data);
-      },
-      error => {
-        // console.error('Error in component:', error);
-      }
-    );
+
+  switchLanguage(language: string): void {
+    this.languageService.setLanguage(language);
   }
-
-  async initializeApp() {
-
-    this.platform.ready().then(async () => {
-      SplashScreen.hide();
-    });
-  }
-
-
 }
