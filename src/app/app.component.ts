958ad105bfce07bf16a15c647a305fe4416473eb
Esra Neuhofer <esraneuhofer@MacBook-Pro-von-Esra.local>
2024-08-28 14:31:16 +0200

    MS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+// import {Component, HostListener, OnInit} from '@angular/core';
+// import { LanguageService } from "./service/language.service";
+// import { ApiService } from "./service/api.service";
+// import { environment } from "../environments/environment";
+// import { ToastingService } from "./service/toastr.service";
+// import { SplashScreen } from '@capacitor/splash-screen';
+// import {Platform} from "@ionic/angular";
+//
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent implements OnInit {
+//
+//   networkListener: any
+//   isOnline: boolean = navigator.onLine;
+//
+//   constructor(private toastr: ToastingService,
+//               private platform: Platform,
+//               private languageService: LanguageService,
+//               private apiService: ApiService) {
+//     // console.log(`Environment API Base URL: ${environment.apiBaseUrl}`);  // Log the environment variable directly
+//   }
+//
+//   switchLanguage(language: string): void {
+//     this.languageService.setLanguage(language);
+//   }
+//
+//
+//   // Event Listener für Netzwerkstatus-Änderungen
+//   @HostListener('window:online', ['$event'])
+//   onOnline(event: Event) {
+//     this.isOnline = true;
+//     console.log('Online!');
+//     alert('Internetverbindung wieder hergestellt.');
+//     // Handle going online
+//   }
+//
+//   @HostListener('window:offline', ['$event'])
+//   onOffline(event: Event) {
+//     this.isOnline = false;
+//     console.log('Offline!');
+//     alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
+//     // Handle going offline
+//   }
+//
+//   updateNetworkStatus() {
+//     if (this.isOnline) {
+//       console.log('The application is online.');
+//     } else {
+//       console.log('The application is offline.');
+//       alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
+//     }
+//   }
+//   ngOnInit() {
+//
+//     this.initializeApp();
+//     console.log('App component initialized!');
+//     this.apiService.setLanguage({ lang: 'en' }).subscribe(
+//       data => {
+//         // console.log('Data received in component:', data);
+//       },
+//       error => {
+//         // console.error('Error in component:', error);
+//       }
+//     );
+//   }
+//
+//   async initializeApp() {
+//
+//     this.platform.ready().then(async () => {
+//       SplashScreen.hide();
+//       this.updateNetworkStatus();
+//     });
+//   }
+//
+//
+// }
+import { Component, HostListener, OnInit } from '@angular/core';
 import { LanguageService } from "./service/language.service";
 import { ApiService } from "./service/api.service";
 import { environment } from "../environments/environment";
 import { ToastingService } from "./service/toastr.service";
 import { SplashScreen } from '@capacitor/splash-screen';
-import {Platform} from "@ionic/angular";
+import { Platform } from "@ionic/angular";
+import { Network } from '@capacitor/network'; // Importieren Sie das Network Plugin
 
 @Component({
   selector: 'app-root',
@@ -13,7 +93,7 @@ import {Platform} from "@ionic/angular";
 })
 export class AppComponent implements OnInit {
 
-  networkListener: any
+  networkListener: any;
   isOnline: boolean = navigator.onLine;
 
   constructor(private toastr: ToastingService,
@@ -27,34 +107,7 @@ export class AppComponent implements OnInit {
     this.languageService.setLanguage(language);
   }
 
-
-  // Event Listener für Netzwerkstatus-Änderungen
-  @HostListener('window:online', ['$event'])
-  onOnline(event: Event) {
-    this.isOnline = true;
-    console.log('Online!');
-    alert('Internetverbindung wieder hergestellt.');
-    // Handle going online
-  }
-
-  @HostListener('window:offline', ['$event'])
-  onOffline(event: Event) {
-    this.isOnline = false;
-    console.log('Offline!');
-    alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
-    // Handle going offline
-  }
-
-  updateNetworkStatus() {
-    if (this.isOnline) {
-      console.log('The application is online.');
-    } else {
-      console.log('The application is offline.');
-      alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
-    }
-  }
   ngOnInit() {
-
     this.initializeApp();
     console.log('App component initialized!');
     this.apiService.setLanguage({ lang: 'en' }).subscribe(
@@ -65,15 +118,31 @@ export class AppComponent implements OnInit {
         // console.error('Error in component:', error);
       }
     );
+
+    // Netzwerkstatus-Listener einrichten
+    this.setupNetworkListener();
   }
 
   async initializeApp() {
-
     this.platform.ready().then(async () => {
       SplashScreen.hide();
       this.updateNetworkStatus();
     });
   }
 
+  setupNetworkListener() {
+    Network.addListener('networkStatusChange', (status) => {
+      this.isOnline = status.connected;
+      this.updateNetworkStatus();
+    });
+  }
 
+  updateNetworkStatus() {
+    if (this.isOnline) {
+      console.log('The application is online.');
+    } else {
+      console.log('The application is offline.');
+      alert('Keine Internetverbindung. Bitte überprüfen Sie Ihre Netzwerkeinstellungen.');
+    }
+  }
 }
