19efdb5329db68c0676792dde654af60d2cee32e
Esra Neuhofer <esraneuhofer@mbp-von-esra.speedport.ip>
2024-09-03 10:48:25 +0200

    ms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent implements OnInit {
     }
   }
   ngOnInit() {
-    SplashScreen.hide();
+
     this.initializeApp();
     console.log('App component initialized!');
     this.apiService.setLanguage({ lang: 'en' }).subscribe(
@@ -68,8 +68,9 @@ export class AppComponent implements OnInit {
   }
 
   async initializeApp() {
+    await SplashScreen.hide();
+
     await this.platform.ready();
-    SplashScreen.hide();
     this.updateNetworkStatus();
   }
 
