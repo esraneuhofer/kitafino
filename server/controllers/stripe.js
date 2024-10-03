d020cb20e22cda6de639d8331dbb8cb32ebe1aa7
Esra Neuhofer <esraneuhofer@mbp-von-esra.speedport.ip>
2024-10-03 14:44:49 +0200

    S

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -39,7 +39,9 @@ exports.createPaymentIntent = async (req, res) => {
 
     // Definiere die Web-URLs, die als Universal/App Links fungieren
     const successUrl = process.env.NODE_ENV === 'production'
-      ? `https://essen.cateringexpert.de/home/account_overview?status=success&amount=${amountPayment}`
+      ` essenapp://home/account_overview?status=success&amount=${amountPayment}`
+        // ? `https://essen.cateringexpert.de/home/account_overview?status=success&amount=${amountPayment}`
+        ? `https://essen.cateringexpert.de/home/account_overview?status=success&amount=${amountPayment}`
       : `http://localhost:4200/home/account_overview?status=success&amount=${amountPayment}`;
 
     const cancelUrl = process.env.NODE_ENV === 'production'
